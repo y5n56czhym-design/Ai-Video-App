3784a47d3c4eafd40e8f4cc2c754e0e1cbd0b177
y5n56czhym-design <y5n56czhym@privaterelay.appleid.com>
2026-05-29 09:17:30 -0400

    Update create-video.js

--- api/create-video.js
+++ api/create-video.js
@@ -1,1 +1,11 @@
+export default async function handler(req, res) {
 
+  const { script, character } = req.body;
+
+  const videoUrl = "https://www.w3schools.com/html/mov_bbb.mp4";
+
+  res.status(200).json({
+    status: "completed",
+    videoUrl
+  });
+}
